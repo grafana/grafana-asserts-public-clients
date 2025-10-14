8d940ddd0a4ac098fccb6abc8793d40865f1a5d8
matt-metivier <29111834+matt-metivier@users.noreply.github.com>
2025-10-14 13:39:21 +0000

    Update clients and publish

--- go/gcom/client.go
+++ go/gcom/client.go
@@ -3,7 +3,7 @@ Asserts, Inc
 
 Asserts Public API
 
-API version: 2025.10.14-114207
+API version: 2025.10.14-153818
 Contact: support@asserts.ai
 */
 
@@ -41,7 +41,7 @@ var (
 	queryDescape    = strings.NewReplacer("%5B", "[", "%5D", "]")
 )
 
-// APIClient manages communication with the Asserts, Inc API v2025.10.14-114207
+// APIClient manages communication with the Asserts, Inc API v2025.10.14-153818
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
