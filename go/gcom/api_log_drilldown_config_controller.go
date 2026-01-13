743192686d51d2e9e843c002bc34332d6f0086d7
matt-metivier <29111834+matt-metivier@users.noreply.github.com>
2026-01-13 01:32:20 +0000

    Update clients and publish

--- go/gcom/api_log_drilldown_config_controller.go
+++ go/gcom/api_log_drilldown_config_controller.go
@@ -3,7 +3,7 @@ Asserts, Inc
 
 Asserts Public API
 
-API version: 2025.12.11-145308
+API version: 2026.01.12-183119
 Contact: support@asserts.ai
 */
 
@@ -219,6 +219,137 @@ func (a *LogDrilldownConfigControllerAPIService) GetTenantLogConfigExecute(r Api
 		newErr := &GenericOpenAPIError{
 			body:  localVarBody,
 			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+type ApiReorderLogConfigPrioritiesRequest struct {
+	ctx                        context.Context
+	ApiService                 *LogDrilldownConfigControllerAPIService
+	reorderLogConfigRequestDto *ReorderLogConfigRequestDto
+	xScopeOrgID                *string
+}
+
+func (r ApiReorderLogConfigPrioritiesRequest) ReorderLogConfigRequestDto(reorderLogConfigRequestDto ReorderLogConfigRequestDto) ApiReorderLogConfigPrioritiesRequest {
+	r.reorderLogConfigRequestDto = &reorderLogConfigRequestDto
+	return r
+}
+
+// Grafana Tenant/Stack ID
+func (r ApiReorderLogConfigPrioritiesRequest) XScopeOrgID(xScopeOrgID string) ApiReorderLogConfigPrioritiesRequest {
+	r.xScopeOrgID = &xScopeOrgID
+	return r
+}
+
+func (r ApiReorderLogConfigPrioritiesRequest) Execute() (*TenantLogConfigResponseDto, *http.Response, error) {
+	return r.ApiService.ReorderLogConfigPrioritiesExecute(r)
+}
+
+/*
+ReorderLogConfigPriorities Reorder log drilldown configuration priorities
+
+Accepts a list of log drilldown configuration names and their new priorities to reorder the configurations for the tenant
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@return ApiReorderLogConfigPrioritiesRequest
+*/
+func (a *LogDrilldownConfigControllerAPIService) ReorderLogConfigPriorities(ctx context.Context) ApiReorderLogConfigPrioritiesRequest {
+	return ApiReorderLogConfigPrioritiesRequest{
+		ApiService: a,
+		ctx:        ctx,
+	}
+}
+
+// Execute executes the request
+//
+//	@return TenantLogConfigResponseDto
+func (a *LogDrilldownConfigControllerAPIService) ReorderLogConfigPrioritiesExecute(r ApiReorderLogConfigPrioritiesRequest) (*TenantLogConfigResponseDto, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodPut
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *TenantLogConfigResponseDto
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "LogDrilldownConfigControllerAPIService.ReorderLogConfigPriorities")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/v2/config/log/reorder"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.reorderLogConfigRequestDto == nil {
+		return localVarReturnValue, nil, reportError("reorderLogConfigRequestDto is required and must be specified")
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/json", "application/x-yml", "application/x-yaml"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json", "application/x-yml", "application/x-yaml"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	if r.xScopeOrgID != nil {
+		parameterAddToHeaderOrQuery(localVarHeaderParams, "X-Scope-OrgID", r.xScopeOrgID, "")
+	}
+	// body params
+	localVarPostBody = r.reorderLogConfigRequestDto
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 422 {
+			var v ApiError
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
